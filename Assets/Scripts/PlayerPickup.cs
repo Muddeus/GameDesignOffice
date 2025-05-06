3d086f8032d3c27480a96677a9a9e2e6af3d98a7
TropicYeti <YetiTropic@gmail.com>
2025-05-06 16:08:48 +1000

    Player Pickup Script, added a pickup distance and pickup time variable to it.

---
+++ Assets/Scripts/PlayerPickup.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour
+{
+    //The distance from which the player can reach the pickup object
+    [SerializeField] private float pickupDistance;
+
+    //how long it takes to pickup the item
+    [SerializeField] private float pickupTime = 1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
